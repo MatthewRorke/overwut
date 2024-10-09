f3f7ed2eecf41ae09bacaa17709ade796b2028e2
Matt Rorke <matthew.rorke@mela.co.uk>
2024-10-09 23:16:19 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -54,42 +54,44 @@ function App() {
     setSelectedCharacter(character);
   }
   return (
-    <div className="flex flex-row">
-      <div>
-        <Character character={selectedCharacter} characterList={characterList} onChangeCharacter={handleChangeCharacter}/>
-      </div>
-      <div className="flex flex-col gap-2 z-10">
-        <div className="flex flex-row gap-2 justify-between">
-          <div className="flex flex-col">
-            <CharacterSwitcher characterList={characterList} onChangeCharacter={handleChangeCharacter}/>
-          </div>
-          <div className="flex flex-row gap-2 justify-end">
-            <div className="hover:cursor-pointer" onClick={() => setIsAdvanced(!isAdvanced)} title={`Change to ${isAdvanced ? 'Simple Mode' : 'Advanced Mode'}`}>
-              <Tag>
-                <h6 className="capitalize">{ isAdvanced ? 'Advanced Mode' : 'Simple Mode' }</h6>
-              </Tag>
-            </div>
-            <Tag title="Base Health">
-              <div className="size-12">
-                <HealthTicker />
-              </div>
-              <h6 className="capitalize">{selectedCharacter.health}</h6>
-            </Tag>
-            <Tag title="Base Shield">
-              <div className="size-12">
-                <HealthTicker type="shield" />
-              </div>
-              <h6 className="capitalize">{selectedCharacter.shield}</h6>
-            </Tag>
-            <Role roleName={selectedCharacter.role} />
-          </div>
+    <div className="container">
+      <div className="flex flex-row">
+        <div>
+          <Character character={selectedCharacter} characterList={characterList} onChangeCharacter={handleChangeCharacter}/>
         </div>
-        <div className="flex flex-col gap-2 justify-between">
-          {
-            selectedCharacter.skills.map((skill, key) => (
-              <Skill key={key} skill={skill} showAdvanced={isAdvanced} />
-            ))
-          }
+        <div className="flex flex-col gap-2 z-10">
+          <div className="flex flex-row gap-2 justify-between">
+            <div className="flex flex-col">
+              <CharacterSwitcher characterList={characterList} onChangeCharacter={handleChangeCharacter}/>
+            </div>
+            <div className="flex flex-row gap-2 justify-end">
+              <div className="hover:cursor-pointer" onClick={() => setIsAdvanced(!isAdvanced)} title={`Change to ${isAdvanced ? 'Simple Mode' : 'Advanced Mode'}`}>
+                <Tag>
+                  <h6 className="capitalize p-4">{ isAdvanced ? 'Advanced Mode' : 'Simple Mode' }</h6>
+                </Tag>
+              </div>
+              <Tag title="Base Health">
+                <div className="size-12">
+                  <HealthTicker />
+                </div>
+                <h6 className="capitalize">{selectedCharacter.health}</h6>
+              </Tag>
+              <Tag title="Base Shield">
+                <div className="size-12">
+                  <HealthTicker type="shield" />
+                </div>
+                <h6 className="capitalize">{selectedCharacter.shield}</h6>
+              </Tag>
+              <Role roleName={selectedCharacter.role} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 justify-between">
+            {
+              selectedCharacter.skills.map((skill, key) => (
+                <Skill key={key} skill={skill} showAdvanced={isAdvanced} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
@@ -154,25 +156,23 @@ function Skill({skill, showAdvanced}) {
   return (
   <div className="flex flex-col w-100">
     <div className="flex flex-row py-4 w-100 menu rounded-t px-4">
-      <div className="flex flex-col w-6/12 justify-center items-center p-4">
-        <div className="w-100 flex items-center">
+      <div className="flex flex-col w-6/12 items-center p-2">
+        <div className="w-100 sticky top-1 items-center">
           <img src={HanzoWallClimb} />
+          <h4 className="w-24 text-lg capitalize font-light">{selectedSkill.name}</h4>
         </div>
-        <h4 className="w-24 text-lg capitalize font-light">{selectedSkill.name}</h4>
       </div>
-      <div className="flex round-full">
-        <div className="flex flex-col justify-center items-start">
-            {selectedSkill.meta.filter(object => showAdvanced || simpleFields.includes(object.key.toLowerCase())).map((object, key) => (
-                <p className="flex flex-row text-sm gap-2" key={key}>
-                  <span className={`font-medium w-[150px] text-right capitalize color-code-${slugIt(object.key)}`}>
-                    {object.key}
-                  </span> |
-                  <span className={`text-left w-[250px]`}>
-                    {object.value}
-                  </span>
-                </p>
-            ))}
-        </div>
+      <div className="flex flex-col grow gap-1">
+        {selectedSkill.meta.filter(object => showAdvanced || simpleFields.includes(object.key.toLowerCase())).map((object, key) => (
+          <div className="flex flex-row justify-start gap-2 menu-dark rounded border-1 text-sm p-2 w-100" key={key}>
+            <p className={`font-medium text-right capitalize color-code-${slugIt(object.key)}`}>
+              {object.key}
+            </p>|
+            <p className={`text-left`}>
+              {object.value}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
     <div className="flex flex-row px-4 py-2 w-100 menu-dark rounded-b gap-8 justify-end items-end">
