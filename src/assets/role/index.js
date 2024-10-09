4d9e7a199b6e8c9f61e1414f94ea1ce8e0b19e88
MatthewRorke <MatthewRorke@users.noreply.github.com>
2024-10-09 18:32:04 +0000

    deploy: 985e7dce527906a580062cd2db382f8ee88c14b3

--- src/assets/role/index.js
+++
@@ -1,9 +0,0 @@
-import Damage from './damage.webp';
-import Support from './support.webp';
-import Tank from './tank.webp';
-
-export const RoleImage = {
-    damage: Damage,
-    support: Support,
-    tank: Tank
-}
