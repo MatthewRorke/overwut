ed0e0ed98dd885271734b4f1120d8946c88da08a
Matt Rorke <matthew.rorke@mela.co.uk>
2024-10-09 21:57:27 +0200

    First Broken Public Release

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,31 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { HeroImages } from './assets/heroes'
 import { ResistanceImage } from './assets/resistances'
+import ZZZ from './assets/zzz.svg';
+import NoEntry from './assets/noentry.svg';
 
 import HanzoWallClimb from './assets/skills/hanzo_wallclimb.webp'
 import Characters from './out/character.json'
 import Skills from './out/skill.json'
 import { RoleImage } from './assets/role'
+
+const simpleFields = [
+  'damage',
+  'health',
+  'headshot',
+  'rate of fire',
+  'cast time',
+  'duration',
+  'area of effect',
+  'max range',
+  'ammo',
+  'movement speed',
+  'reload time',
+  'healing',
+  'healing modification',
+  'dmg. amplification',
+  'projectile speed'
+]
 
 function CharacterSwitcher({characterList, onChangeCharacter}) {
   return (
@@ -20,6 +40,8 @@ function CharacterSwitcher({characterList, onChangeCharacter}) {
 
 function App() {
   const [selectedCharacter, setSelectedCharacter] = useState(Characters[1]);
+  const [isAdvanced, setIsAdvanced] = useState(true);
+
   const characterList = Characters.map((character) => (
     character.name
   )).filter((_, index) => index != 0).sort();
@@ -36,11 +58,16 @@ function App() {
         <Character character={selectedCharacter} characterList={characterList} onChangeCharacter={handleChangeCharacter}/>
       </div>
       <div className="flex flex-col gap-2 z-10">
-        <div className="flex flex-row gap-2 justify-between mr-1">
+        <div className="flex flex-row gap-2 justify-between">
           <div className="flex flex-col">
             <CharacterSwitcher characterList={characterList} onChangeCharacter={handleChangeCharacter}/>
           </div>
           <div className="flex flex-row gap-2 justify-end">
+            <div className="hover:cursor-pointer" onClick={() => setIsAdvanced(!isAdvanced)}>
+              <Tag>
+                <h6 className="capitalize">{ isAdvanced ? 'Advanced Mode' : 'Simple Mode' }</h6>
+              </Tag>
+            </div>
             <Tag>
               <div className="size-12">
                 <HealthTicker />
@@ -56,11 +83,13 @@ function App() {
             <Role roleName={selectedCharacter.role} />
           </div>
         </div>
+        <div className="flex flex-col gap-2 justify-between">
           {
             selectedCharacter.skills.map((skill, key) => (
-              <Skill key={key} skill={skill} />
+              <Skill key={key} skill={skill} showAdvanced={isAdvanced} />
             ))
           }
+        </div>
       </div>
     </div>
   )
@@ -115,15 +144,15 @@ function Character({character, characterList, onChangeCharacter}) {
   );
 }
 
-function Skill({skill}) {
+function Skill({skill, showAdvanced}) {
   const selectedSkill = Skills.find(skills => skills.name === skill);
   const slugIt = (name) => {
     return name.replaceAll(' ', '-').replaceAll('.', '').toLowerCase();
   }
-  if(!selectedSkill) return <></>
+  if(!selectedSkill) return null;
   return (
   <div className="flex flex-row gap-2 w-100">
-    <div className="flex flex-row py-4 w-10/12 menu rounded pr-4">
+    <div className="flex flex-row py-4 w-11/12 menu rounded pr-4">
       <div className="flex flex-col justify-center items-center p-4">
         <div className="size-12">
           <img src={HanzoWallClimb} />
@@ -132,9 +161,9 @@ function Skill({skill}) {
       </div>
       <div className="flex round-full">
         <div className="flex flex-col justify-center items-start">
-            {selectedSkill.meta.map((object, key) => (
+            {selectedSkill.meta.filter(object => showAdvanced || simpleFields.includes(object.key.toLowerCase())).map((object, key) => (
                 <p className="flex flex-row text-sm gap-2" key={key}>
-                  <span className={`font-medium w-[125px] text-right capitalize color-code-${slugIt(object.key)}`}>
+                  <span className={`font-medium w-[150px] text-right capitalize color-code-${slugIt(object.key)}`}>
                     {object.key}
                   </span> |
                   <span className={`text-left w-[250px]`}>
@@ -145,7 +174,7 @@ function Skill({skill}) {
         </div>
       </div>
     </div>
-    <div className="flex flex-col py-4 w-24 menu rounded gap-8 justify-between items-center">
+    <div className="flex flex-col py-4 w-1/12 menu rounded gap-8 justify-between items-center">
         {
           selectedSkill.resistances.filter((resistance) => resistance).map((resistance, key) => (<Resistance key={key} imageName={(resistance.replaceAll(' ', '_').replaceAll('\/', '_')).toLowerCase()} tooltip={resistance} />))
         }
@@ -155,18 +184,45 @@ function Skill({skill}) {
 }
 
 function Resistance({imageName, tooltip}) {
-  // const statusCheck = [
-  //   "ignores",
-  //   "partially",
-  //   "blocked"
-  // ]
-  // statusCheck.forEach((status) => {
-
-  // })
+  //const [selectedStatus, setSelectedStatus] = useState(<img src={ResistanceImage[imageName]} title={tooltip} />);
+  const statusCheck = {
+    ignores: {
+      extraClass: (
+      <div className="relative">
+        <img src={ResistanceImage[imageName]} title={tooltip} className="opacity-90" />
+        <img src={ZZZ} className="absolute top-0 right-0" title={tooltip} width="24"/>
+      </div>
+      )
+    },
+    partially: {
+      extraClass: (
+        <div className="relative">
+          <img src={ResistanceImage[imageName]} title={tooltip} className="opacity-90" />
+          <img src={NoEntry} className="absolute top-0 right-0 opacity-50" title={tooltip}  width="24"/>
+        </div>
+      )
+    },
+    blocked: {
+      extraClass: (
+        <div className="relative">
+          <img src={ResistanceImage[imageName]} title={tooltip} />
+        </div>
+      )
+    },
+  };
+  // hacky handling
+  let selectedStatusEl = <img src={ResistanceImage[imageName]} title={tooltip} />
+  let selectedStatus = null;
+  Object.keys(statusCheck).forEach((status) => {
+    if(imageName.indexOf(status) > -1) {
+      selectedStatusEl = statusCheck[status].extraClass;
+      selectedStatus = status;
+    }
+  });
 
   return (
-    <div className="flex flex-col">
-      <img src={ResistanceImage[imageName]} title={tooltip}/>
+    <div className={`flex flex-col`}>
+      {selectedStatusEl} 
     </div>
   )
 }
